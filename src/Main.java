527ee7f03ae295a7ea1c960ab14de44962a6482e
anuradha <anuranasinghe151@gmail.com>
2019-07-04 22:24:56 +0530

    int stream

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.stream.IntStream;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IntStream.range(1,20).forEach(System.out::println);
     }
 }
